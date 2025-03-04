31bcd6980cde613f128301039545ce565b9f4979
nicola <battaglinonicola@outlook.com>
2025-03-04 21:50:36 +0100

    modify logo and add slider homepage

--- script1.js
+++ script1.js
@@ -6,3 +6,17 @@ document.addEventListener("DOMContentLoaded", function () {
             document.body.insertAdjacentHTML("afterbegin", data);
         });
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let slides = document.querySelectorAll(".slide");
+    let currentIndex = 0;
+
+    function changeSlide() {
+        slides[currentIndex].classList.remove("active");
+        currentIndex = (currentIndex + 1) % slides.length;
+        slides[currentIndex].classList.add("active");
+    }
+
+    setInterval(changeSlide, 3000); // Cambia immagine ogni 3 secondi
+});
